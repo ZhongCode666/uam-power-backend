7731e2cb573408a50bc344290b320894fc9d2a0b
Hunter <2662534371@qq.com>
2024-11-20 13:27:20 +0800

    fix: 优化transfer启动逻辑again🤮

--- main.go
+++ main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
 	"uam-power-backend/routes"
-	"uam-power-backend/service/data_transfer_service"
 	"uam-power-backend/utils"
 )
 
 func main() {
 	GlobalConfigPath := "config/global_config.yaml"
 	GlobalCfg, loadCfgErr := utils.LoadGlobalConfig(GlobalConfigPath)
-	data_transfer_service.StartDataTransferService(GlobalConfigPath)
+	RedisTransfer, _ := filepath.Abs("service/data_transfer_service/kafka_to_redis.go")
+	MySqlTransfer, _ := filepath.Abs("service/data_transfer_service/kafka_to_mysql.go")
+	currentDir, _ := os.Getwd()
+	for i := 0; i < GlobalCfg.KafkaPartitionNum; i++ {
+		cmd := exec.Command("go", "run", RedisTransfer, GlobalCfg.DBConfigPath)
+		cmd.Dir = currentDir
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Start()
+		if err != nil {
+			return
+		}
+	}
+
+	for i := 0; i < GlobalCfg.KafkaPartitionNum; i++ {
+		cmd := exec.Command("go", "run", MySqlTransfer, GlobalCfg.DBConfigPath)
+		cmd.Dir = currentDir
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Start()
+		if err != nil {
+			return
+		}
+	}
+	utils.MsgSuccess("[main_transfer_server]init transfer service successfully!")
 	if loadCfgErr != nil {
 		return
 	}
