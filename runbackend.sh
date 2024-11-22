9a3593f2def4bc724693a73410a6d2dd3b59d3a1
root <root@localhost.localdomain>
2024-11-22 17:19:35 +0800

    fix: 修复sh脚本bug:D

--- runbackend.sh
+++ runbackend.sh
@@ -4,15 +4,15 @@
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # 创建日志文件夹
-log_dir=" ~/uam-power-backend/logs/$timestamp"
+log_dir="$HOME/uam-power-backend/logs/$timestamp"
 mkdir -p "$log_dir"
 
 # 定义要运行的 nohup 命令数组
 commands=(
-    "nohup ~/uam-power-backend/id_route_main > $log_dir/id_route_main.log 2>&1 &"
-    "nohup ~/uam-power-backend/task_route_main > $log_dir/task_route_main.log 2>&1 &"
-    "nohup ~/uam-power-backend/data_route_main > $log_dir/data_route_main.log 2>&1 &"
-    "nohup ~/uam-power-backend/transfer_main > $log_dir/transfer_main.log 2>&1 &"
+    "nohup $HOME/uam-power-backend/id_route_main > $log_dir/id_route_main.log 2>&1 &"
+    "nohup $HOME/uam-power-backend/task_route_main > $log_dir/task_route_main.log 2>&1 &"
+    "nohup $HOME/uam-power-backend/data_route_main > $log_dir/data_route_main.log 2>&1 &"
+    "nohup $HOME/uam-power-backend/transfer_main > $log_dir/transfer_main.log 2>&1 &"
 )
 
 # 循环执行每个 nohup 命令
